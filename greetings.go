66982f5df5c2b516580414f01f6ee264f0d7f8b4
Raymond Chen <raymond.chen@mlb.com>
2022-10-02 16:15:45 -0400

    typo on fmnt.Sprintf

--- greetings.go
+++ greetings.go
@@ -3,6 +3,6 @@ package greetings
 import "fmt"
 
 func Hello(name string) string {
-  message := fmt.Sprint("Hi, %v.  Welcome!", name)
+  message := fmt.Sprintf("Hi, %v.  Welcome!", name)
   return message
 }
